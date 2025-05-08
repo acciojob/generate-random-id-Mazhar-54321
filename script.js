fadc0c2feba4ef1d1d584e358e98d178f931812b
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-08 08:26:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeid(l) {
   // write your code here
+	 const allowedCharacters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let res=""
+  while(l>0){
+	l--;
+	let random = Math.floor(Math.random()*62);
+	//console.log(random)
+	res+=allowedCharacters[random]
+  }
+  return res;
 }
 
 // Do not change the code below.
